1fee3829de0aad3b097cf80e86e31e18f3e4ea86
Miftahul Iman <101625333+MiftahulIman15@users.noreply.github.com>
2022-04-13 05:09:36 +0700

    Add files via upload

---
+++ studi kasus3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Operator {
+	public:
+		long pengeluaran();
+		
+	private:
+  long int beli, uang, tabungan, sisa, jumlah;
+};
+
+long Operator::pengeluaran(){
+ int i,n,input1,input2;
+  for(int i=1; i<=n; i++){
+    input1= input1-input2;
+  }
+return input1;
+};
+
+int main(){
+  Operator x;
+  cout<<x.pengeluaran();
+}
