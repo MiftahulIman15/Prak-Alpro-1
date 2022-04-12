8805fa49f387641cc03bfdba50cc33433bace885
Miftahul Iman <101625333+MiftahulIman15@users.noreply.github.com>
2022-04-13 05:12:49 +0700

    Update studi kasus3.cpp

--- studi kasus3.cpp
+++ studi kasus3.cpp
@@ -4,20 +4,41 @@ using namespace std;
 class Operator {
 	public:
 		long pengeluaran();
-		
+    long totalsemua();
+    long tabungan();
 	private:
-  long int beli, uang, tabungan, sisa, jumlah;
+  long int input1;
+  long int total;
 };
 
 long Operator::pengeluaran(){
  int i,n,input1,input2;
   for(int i=1; i<=n; i++){
-    input1= input1-input2;
+    input1= 10000-5000;
   }
-return input1;
+  return input1;
 };
-
+long Operator::totalsemua(){
+ int i,n,total;
+  total=5000*30;
+  for(int i=1; i<=n; i++){
+   total=5000*30;
+  }
+return total;
+};
+long Operator::tabungan(){
+ int n,totaltabungan;
+  for(int i=1; i<=n; i++){
+   totaltabungan=5000*30;
+  }
+return totaltabungan;
+};
 int main(){
   Operator x;
-  cout<<x.pengeluaran();
+  cout<<"Pengeluaran Tiap Bulan : "<<x.pengeluaran();
+  cout<<endl;
+  cout<<"Total Pengeluaran\t : "<<x.totalsemua();
+  cout<<endl;
+  cout<<"Tabungan\t\t\t : "<<x.tabungan();
+  cout<<endl;
 }
