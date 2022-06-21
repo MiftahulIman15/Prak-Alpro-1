47728e2bd13845a5869263a8e44ff3d0eaef6748
Miftahul Iman <101625333+MiftahulIman15@users.noreply.github.com>
2022-06-21 15:59:27 +0700

    Update prak alpro 10.cpp

--- prak alpro 10.cpp
+++ prak alpro 10.cpp
@@ -1,47 +1,31 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+const int nim =5;
+class hitung{
+	public:
+		void proses();
+	private:
+	int A [nim];
+	string nama[10];
+};
 
- class node {
- public :
- int data;
- node *berikut;
- };
+void hitung::proses(){
+		int *pInt=A;
+	 for (int i=0; i<nim; i++)
+    {
+        cout<<"Input nim : ";cin>>A[i];
+        cout<<"Input Nama : ";cin>>nama[i];
+    }
+	
+    for (int n=0; n<nim; n++)
+    {
+    	cout<<"Nama Mahasiswa : "<<nama[n]<<endl;
+        cout<<"Nim Mahasiswa :" <<*(pInt)<<endl;
+        pInt++;
+    }
+}
 
-int main() {
- // langkah satu
- node *baru;
- baru = new node;
- baru->data = 5;
- baru->berikut = NULL;
- cout << "Isi data node baru adalah : " << baru->data << endl;
- // langkah dua
- node *lain;
- lain = new node;
- lain->data = 6;
- lain->berikut = NULL;
- cout << "Isi data node lain adalah : " << lain->data << endl;
- // langkah tiga : menyambung rantai
- baru->berikut = lain;
- cout << "Isi data node lain dicetak dari node baru adalah : ";
-	cout << baru->berikut->data << endl;
- // langkah empat
- node *kepala = baru;
- cout << "Mencetak node pertama dari pointer kepala : ";
- cout << kepala->data << endl;
- cout << "Mencetak node kedua dari pointer kepala : ";
- cout << kepala->berikut->data << endl;
- // langkah lima : pointer yang jalan-jalan
- cout << "Menggunakan perulangan untuk mencetak setiap data pada rantai\n";
- node *jalan = kepala;
- int i = 1;
- while (jalan != NULL) {
- cout << "Data ke-" << i << " > " << jalan->data << endl;
- i++;
- jalan = jalan->berikut;
- }
- // langkah enam : bukti bahwa pointer kepala tidak kehilangan data
- cout << "Mencetak node pertama dari pointer kepala : ";
- cout << kepala->data << endl;
- cout << "Mencetak node kedua dari pointer kepala : ";
- cout << kepala->berikut->data << endl;
- }
+int main(){
+	hitung x;
+	x.proses();
+}
